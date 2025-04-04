4177f027a563f390b9817f987d0890141e1e5887
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2025-04-04 18:34:32 +0100

    step 5 added a simple react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello World</h1>
+      <p>Welcome to the Color Import Application</p>
+    </div>
+  )
+}
+
+export default App
